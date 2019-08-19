b3415542db6ef327670ce1b613dd9fc4be16c3b5
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-08-20 08:30:11 +0900

    Fix declaration variable and function

--- Inc/stm32f3_access_flash_byte.hpp
+++ Inc/stm32f3_access_flash_byte.hpp
@@ -1,27 +1,20 @@
 #ifndef STM32F3_ACCESS_FLASH_BYTE_HPP_
 #define STM32F3_ACCESS_FLASH_BYTE_HPP_
 
+#include "stm32f3xx_hal.h"
+
 class Stm32f3AccessFlashByte
 {
     private:
-        int index;
-        unsigned char data;
-        int is_appropriate_index(int index);
+        int index_;
+        unsigned char data_;
+        int is_appropriate_index(int);
 
     public:
-        Stm32f3AccessFlashByte(int index);
+        Stm32f3AccessFlashByte(int);
         ~Stm32f3AccessFlashByte();
         unsigned char get();
         void save();
 };
-
-Stm32f3AccessFlashByte::Stm32f3AccessFlashByte(int index)
-{
-}
-
-Stm32f3AccessFlashByte::~Stm32f3AccessFlashByte()
-{
-}
-
 
 #endif // STM32F3_ACCESS_FLASH_BYTE_HPP_
