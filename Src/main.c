778ea1f39c651c20ca207b85a2898b344bac79a6
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-09-14 10:43:07 +0900

    Include cppmain.hpp

--- Src/main.c
+++ Src/main.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "cppmain.hpp"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
