142fe404e6df82bc9073da48596faddfb01a50e5
Hajime <hajime0316@s.okayama-u.ac.jp>
2019-09-14 11:52:51 +0900

    Access flash using new module

--- Src/cppmain.cpp
+++ Src/cppmain.cpp
@@ -10,9 +10,12 @@
 static uint8_t sw_sum;
 static int led_flag = 0;
 
+Stm32AccessFlashByte* memory;
+
 void setup() {
     HAL_TIM_Base_Start_IT(&htim6);
-    sw_sum = HAL_FLASHEx_OBGetUserData(OB_DATA_ADDRESS_DATA0);
+    memory = new Stm32AccessFlashByte(0);
+    sw_sum = memory->get();
 }
 
 void loop() {
@@ -24,24 +27,7 @@ void loop() {
         sw_sum++;
 
         // eepromにsw_sumを記録
-        uint32_t address;
-        uint8_t data;
-        FLASH_OBProgramInitTypeDef OBInit;
-
-        address = OB_DATA_ADDRESS_DATA0;
-        data = sw_sum;
-
-        HAL_FLASHEx_OBGetConfig(&OBInit);
-        OBInit.OptionType = OPTIONBYTE_DATA | OPTIONBYTE_USER | OPTIONBYTE_DATA;
-        OBInit.DATAAddress = address;
-        OBInit.DATAData = data;
-        
-        HAL_FLASH_Unlock();
-        HAL_FLASH_OB_Unlock();
-        HAL_FLASHEx_OBErase();    // これがないとProgram出来ない．
-        HAL_FLASHEx_OBProgram(&OBInit);
-        HAL_FLASH_OB_Lock();
-        HAL_FLASH_Lock();
+        memory->save(sw_sum);
     }
     else if (HAL_GPIO_ReadPin(SW_GPIO_Port, SW_Pin) == GPIO_PIN_RESET && led_flag == 1) {
         HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_SET);
